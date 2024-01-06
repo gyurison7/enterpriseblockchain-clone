b26d2520c611c699902d73770ef0c0fb85738643
gyurison7 <orangetree401@gmail.com>
2024-01-07 06:09:22 +0900

    feat: 엔터프라이즈블록체인 클론코딩

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ ScrollTrigger.create({
   start: "90% 0%",
   end: "100% 100%",
   scrub: true,
-  // markers: true,
   onEnter: function () {
     gsap.to(".header", {
       duration: 0.5,
@@ -27,7 +26,6 @@ let introMotion = gsap.timeline({
     end: () =>
       "+=" + document.querySelector(".sc-intro").offsetHeight + " bottom", // 섹션의 높이만큼 스크롤
     scrub: true,
-    // markers: true,
   },
 });
 document
@@ -48,7 +46,6 @@ gsap.to(".sc-intro-bg", {
       "+=" + document.querySelector(".sc-intro").offsetHeight + " bottom",
     scrub: true,
     toggleActions: "play none none reverse",
-    // markers: true
   },
   opacity: 1,
 });
@@ -60,7 +57,6 @@ let showcaseMotion = gsap.timeline({
     start: "10% 0%",
     end: "100% 0%",
     scrub: true,
-    // markers: true,
   },
 });
 showcaseMotion
@@ -78,7 +74,6 @@ gsap.to(".showcase-description", {
     end: "100% 0%",
     scrub: true,
     toggleActions: "play none none reverse",
-    // markers: true,
   },
   opacity: 1,
 });
@@ -89,7 +84,6 @@ ScrollTrigger.create({
   start: "0% 10%",
   end: "50% 100%",
   scrub: true,
-  // markers: true,
   onEnter: function () {
     $(".logo-img").addClass("black");
     $(".gnb").addClass("black");
@@ -107,28 +101,30 @@ let proveMotion = gsap.timeline({
     start: "0% 70%",
     end: "100% 0%",
     scrub: true,
-    // markers: true,
   },
 });
-proveMotion.to(".prove-box-before", { width: "68%" }, "motion");
-proveMotion.to(".prove-box-after", { width: "68%" }, "motion");
-proveMotion.to(".prove-content p:first-child", { x: -200 }, "motion");
-proveMotion.to(".prove-content p:last-child", { x: 500 }, "motion");
+proveMotion
+  .to(".prove-box-before", { width: "68%" }, "motion")
+  .to(".prove-box-after", { width: "68%" }, "motion")
+  .to(".prove-content p:first-child", { x: -200 }, "motion")
+  .to(".prove-content p:last-child", { x: 500 }, "motion");
 
 // sc-possible
 ScrollTrigger.create({
   trigger: ".sc-possible",
   start: "0% 60%",
-  end: "50% 50%",
+  end: "100% 0%",
   scrub: true,
   // markers: true,
   onEnter: function () {
+    $(".container").addClass("dark");
     $(".sc-gallery").addClass("dark");
     $(".sc-possible").addClass("dark");
     $(".logo-img").removeClass("black");
     $(".gnb").removeClass("black");
   },
   onLeaveBack: function () {
+    $(".container").removeClass("dark");
     $(".sc-gallery").removeClass("dark");
     $(".sc-possible").removeClass("dark");
     $(".logo-img").addClass("black");
@@ -136,18 +132,65 @@ ScrollTrigger.create({
   },
 });
 
-gsap.to(".possible-wrapper", {
+// possible-slide 가로 이동
+gsap.to(".possible-slide", {
   scrollTrigger: {
     trigger: ".sc-possible",
-    start: "5% 5%",
-    end: () =>
-      "+=" + document.querySelector(".sc-possible").offsetHeight + "px", // 세로 스크롤 길이에 따른 종료 지점
+    start: "0% 0%",
+    end: "50% 0%",
     scrub: true,
     toggleActions: "play none none reverse",
+    // markers: true,
+  },
+  x: "-40%",
+});
+
+// sc-banner
+ScrollTrigger.refresh();
+let bannerMotion = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".sc-banner",
+    start: "0% 100%",
+    end: "100% 0%",
+    scrub: true,
     markers: true,
   },
-  x: () =>
-    -document.querySelector(".possible-wrapper").offsetWidth +
-    window.innerWidth +
-    "px", // 가로로 이동할 거리
 });
+bannerMotion
+  .from(".box-container .box:first-child", { x: -300 }, "motion")
+  .from(".box-container .box:nth-child(2)", { x: -300 }, "motion")
+  .from(".box-container .box:last-child", { x: 300 }, "motion")
+  .to(".banner-content", { opacity: 1 }, "motion2")
+  .to(".box-container .box", { filter: "blur(50px)" }, "motion2");
+
+// safe-slide 가로 이동
+gsap.to(".safe-slide", {
+  scrollTrigger: {
+    trigger: ".safe-slide",
+    start: "10% 0%",
+    end: "300% 0%",
+    scrub: true,
+    // markers: true,
+  },
+  x: "-10%",
+});
+
+// safe-slide 가로 이동
+let boxMotion = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".safe-slide",
+    start: "200% 0%",
+    end: "600% 0%",
+    scrub: true,
+    // markers: true,
+  },
+});
+boxMotion
+  .to(".card-item2", { x: "-100%" }, "motion")
+  .to(".card-item3", { x: "-200%" }, "motion")
+  .to(".card-item4", { x: "-300%" }, "motion")
+  .to(".card-item2", { x: "-=40px" }, "motion")
+  .to(".card-item3", { x: "-=80px" }, "motion")
+  .to(".card-item4", { x: "-=120px" }, "motion")
+  .to(".icon-unlock", { opacity: 0 }, "motion+=0.1")
+  .to(".icon-lock", { opacity: 1 }, "motion+=0.1");
